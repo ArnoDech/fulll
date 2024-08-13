17f9474a90f9b80cf84ca5ef7a074c01cbacacd6
Arnaud-Deschamp <arnaud.deschamp-external@ceva.com>
2024-08-13 16:49:26 +0200

    test

---
+++ Algo/FizzBuzz.py
@@ -0,0 +1,35 @@
+class FizzBuzz:
+    def __init__(self):
+        self.number = 0
+
+    def get_number(self):
+        while True:
+            try:
+                self.number = int(input("Veuillez insérer un nombre entre 0 et N : "))
+                break
+            except ValueError:
+                print("Error : Please insert valid number")
+
+    def fizz_buzz(self):
+        if self.number % 3 == 0 and self.number % 5 == 0:
+            return "FizzBuzz"
+        elif self.number % 3 == 0:
+            return "Fizz"
+        elif self.number % 5 == 0:
+            return "Buzz"
+        else:
+            return "no Fizz & no Buzz"
+
+    def run(self):
+        while True:
+            self.get_number()
+            if self.number < 0:
+                print("End of the loop")
+                break
+            result = self.fizz_buzz()
+            print(result)
+            # Game stopped if number < 0
+
+if __name__ == '__main__':
+    test = FizzBuzz()
+    test.run()
